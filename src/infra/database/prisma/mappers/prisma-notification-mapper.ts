d1cffb5b339bfed08499a891de0f820711d8e51a
Gabriel Brum <grodrigues@Gabriel-Brum.localdomain>
2022-12-18 17:22:09 -0300

    finish

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,6 +1,9 @@
+import { Content } from '@application/entities/content';
 import { Notification } from '@application/entities/notification';
+import { Notification as RawNotification } from '@prisma/client';
 
 //when static, it does not require to instantiate the class
+//converts the application Notification to prisma Notification
 export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
     return {
@@ -11,5 +14,20 @@ export class PrismaNotificationMapper {
       readAt: notification.readAt,
       createdAt: notification.createdAt,
     };
+  }
+
+  //converts prisma Notification to application (domain) Notification
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        canceledAt: raw.canceledAt,
+        createdAt: raw.createdAt,
+      },
+      raw.id,
+    );
   }
 }
